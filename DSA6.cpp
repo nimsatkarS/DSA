7d3faf98121a87218aa6724033ada9bd068625ee
Sanket_Nimsatkar <121743873+nimsatkarS@users.noreply.github.com>
2023-06-18 10:12:53 +0530

    Merge sort

---
+++ DSA6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+mergSort(int n,arr[],beg,end){
+    if (i<n)
+    {
+        /* code */
+    }
+    
+}
+int main(){
+int n;
+cin>>n;
+
+int arr[n];
+for (int i = 0; i <n; i++)
+{
+cout<<arr[i];
+}
+
+return 0;
+}
